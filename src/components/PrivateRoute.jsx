8b76eb09e6efa91dfe7f7c9773e259a8cca2ed3a
Sodara Sou <sodarasou168@gmail.com>
2023-09-17 12:07:37 +0700

    Removed loading spinner

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase.config";
 import { useState, useEffect } from "react";
-import Spinner from "../components/Spinner";
+// import Spinner from "../components/Spinner";
 
 const PrivateRoute = () => {
   const [loggedIn, setLoggedIn] = useState(true);
@@ -12,7 +12,7 @@ const PrivateRoute = () => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoggedIn(true);
-        setCheckStatus(false);
+        // setCheckStatus(false);
       } else {
         setLoggedIn(false);
       }
