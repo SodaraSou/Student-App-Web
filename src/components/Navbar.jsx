2cc7dbb24938a1e1783cb8384d0929e232980aaf
Sodara Sou <sodarasou168@gmail.com>
2023-09-18 11:41:39 +0700

    New navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,79 +1,51 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../components/assets/splash.png";
+import StudentAppContext from "./context/StudentAppContext";
+
 function Navbar() {
+  const { logOut } = useContext(StudentAppContext);
+
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Student-App-Admin
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
+    <>
+      <nav className="navbar navbar-expand-md navbar-light fixed-top bg-cyan">
+        <div className="container">
+          <Link to="/" className="navbar-brand d-flex align-items-center">
+            <img
+              src={Logo}
+              alt="Logo"
+              width={50}
+              height={40}
+              className="me-2"
             />
-            <button className="btn btn-outline-success" type="submit">
-              Search
+            Student App Admin
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item"></li>
+            </ul>
+            <button
+              className="btn btn-outline-danger"
+              type="submit"
+              onClick={logOut}
+            >
+              Sign Out
             </button>
-          </form>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
