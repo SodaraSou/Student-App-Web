ec53074dc6eed0bd9776dc6eae6915c9d1cdab93
Sodara Sou <sodarasou168@gmail.com>
2023-09-24 23:16:21 +0700

    Finished signin page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase.config";
-import test from "../components/assets/splash.png";
+import Logo from "../components/assets/splash.png";
 function SignIn() {
   const [formData, setFormData] = useState({
     email: "",
@@ -34,34 +34,49 @@ function SignIn() {
 
   return (
     <>
-      <form className="form-signin" onSubmit={onsubmit}>
-        <img className="mb-4" src={test} alt="" width="120" height="100" />
-        <h1 className="h3 mb-3 fw-normal">Please Login In</h1>
-
-        <div className="form-floating w-100">
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="name@example.com"
-            onChange={onChange}
-          />
-          <label htmlFor="floatingInput">Email address</label>
+      <div className="min-h-screen flex flex-col justify-center items-center gap-4 p-4 md:p-0">
+        <img src={Logo} alt="Logo" width={100} height={100} />
+        <div className="w-full md:w-96 h-auto shadow-xl rounded-2xl">
+          <h1 className="text-2xl font-semibold p-4 text-center">Sign In</h1>
+          <form className="px-4" onSubmit={onsubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+                onChange={onChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+                onChange={onChange}
+              />
+            </div>
+            <button className="text-sm font-medium w-full bg-cyan py-2.5 rounded-md mb-4">
+              Sign In
+            </button>
+          </form>
         </div>
-        <div className="form-floating w-100">
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
-            onChange={onChange}
-          />
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Login
-        </button>
-      </form>
+      </div>
     </>
   );
 }
