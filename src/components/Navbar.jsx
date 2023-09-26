e870ce5966dd644e06e567e5f09d4a98d49a9857
Sodara Sou <sodarasou168@gmail.com>
2023-09-26 10:38:05 +0700

    Redesigned navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,43 +8,20 @@ function Navbar() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-md navbar-light fixed-top bg-cyan">
-        <div className="container">
-          <Link to="/" className="navbar-brand d-flex align-items-center">
-            <img
-              src={Logo}
-              alt="Logo"
-              width={50}
-              height={40}
-              className="me-2"
-            />
-            Student App Admin
+      <div className="bg-cyan">
+        <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
+          <Link to="/" className="flex items-center gap-4">
+            <img src={Logo} alt="Logo" width={50} height={40} />
+            <h1 className="text-2xl font-semibold">Student App Admin</h1>
           </Link>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
-            aria-controls="navbarCollapse"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            className="bg-red-500 py-2 px-4 rounded-full font-semibold"
+            onClick={logOut}
           >
-            <span className="navbar-toggler-icon"></span>
+            Sign Out
           </button>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item"></li>
-            </ul>
-            <button
-              className="btn btn-outline-danger"
-              type="submit"
-              onClick={logOut}
-            >
-              Sign Out
-            </button>
-          </div>
         </div>
-      </nav>
+      </div>
     </>
   );
 }
