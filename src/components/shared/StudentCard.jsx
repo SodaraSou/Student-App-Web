cc69a3854534f6d8e14e058ecfd433f5b0366eb5
Sodara Sou <sodarasou168@gmail.com>
2023-09-26 10:38:36 +0700

    Minor fixed

--- src/components/shared/StudentCard.jsx
+++ src/components/shared/StudentCard.jsx
@@ -1,20 +1,8 @@
-import React from "react";
+// import Logo from "../assets/splash.png";
 
 function StudentCard() {
   return (
-    <div className="p-3 bg-cyan rounded-4 d-flex align-items-center justify-content-between student-card">
-      <img
-        src=""
-        className="rounded-circle bg-danger"
-        height={100}
-        width={100}
-        alt="logo"
-      />
-      <div>
-        <h3>Hello</h3>
-        <p>Dara</p>
-      </div>
-    </div>
+    <></>
   );
 }
 
